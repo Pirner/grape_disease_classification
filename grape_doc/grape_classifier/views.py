61e3d92c19bd2fc7bdbb5d7db0a3914d6bc26b1f
Nerolith <stephan.pirner93@gmail.com>
2023-02-27 21:56:37 +0100

    add detail view

--- grape_doc/grape_classifier/views.py
+++ grape_doc/grape_classifier/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, grape_classification_id):
+    return HttpResponse("You're looking at question %s." % grape_classification_id)
