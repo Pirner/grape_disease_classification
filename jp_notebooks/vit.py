0e999eb686103f7ff4ecd11604fd730415ec6014
Nerolith <stephan.pirner93@gmail.com>
2023-06-18 14:39:07 +0200

    addd vit for pytorch from scratch

--- jp_notebooks/vit.py
+++ jp_notebooks/vit.py
@@ -1,6 +1,5 @@
 import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
-import glob
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 import torch
 import torch.nn.functional as F
@@ -13,15 +12,34 @@ from torchvision.transforms import Compose, Resize, ToTensor
 from einops import rearrange, reduce, repeat
 from einops.layers.torch import Rearrange, Reduce
 from torchsummary import summary
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+
+
+def create_vit():
+    pass
 
 
 def main():
-    im_paths = glob.glob(os.path.join(r'C:\data\grape_disease\train', '**/*.JPG'), recursive=True)
-    img = Image.open(im_paths[0])
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    fig = plt.figure()
-    plt.imshow(img)
-    plt.show()
+    batch_size = 4
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
 if __name__ == '__main__':
